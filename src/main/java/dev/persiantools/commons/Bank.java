f3808ca4d88b83dbbce600aa95eeda2adf226120
Ebi Ahmadi <eahmadi641@gmail.com>
2021-03-28 01:20:44 +0200

    style(core): style code using prettier

--- src/main/java/dev/persiantools/commons/Bank.java
+++ src/main/java/dev/persiantools/commons/Bank.java
@@ -3,64 +3,65 @@ package dev.persiantools.commons;
 import java.util.List;
 
 public class Bank {
-    private String nickname;
-    private String name;
-    private String persianName;
-    private String ibanCode;
-    private List<Integer> cardNumberIdentifiers;
-    private boolean accountNumberAvailable;
 
-    public String getNickname() {
-        return nickname;
-    }
+  private String nickname;
+  private String name;
+  private String persianName;
+  private String ibanCode;
+  private List<Integer> cardNumberIdentifiers;
+  private boolean accountNumberAvailable;
 
-    public String getName() {
-        return name;
-    }
+  public String getNickname() {
+    return nickname;
+  }
 
-    public String getPersianName() {
-        return persianName;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public List<Integer> getCardNumberIdentifiers() {
-        return cardNumberIdentifiers;
-    }
+  public String getPersianName() {
+    return persianName;
+  }
 
-    public String getIbanCode() {
-        return ibanCode;
-    }
+  public List<Integer> getCardNumberIdentifiers() {
+    return cardNumberIdentifiers;
+  }
 
-    public boolean getAccountNumberAvailable() {
-        return accountNumberAvailable;
-    }
+  public String getIbanCode() {
+    return ibanCode;
+  }
 
-    public Bank setNickname(String nickname) {
-        this.nickname = nickname;
-        return this;
-    }
+  public boolean getAccountNumberAvailable() {
+    return accountNumberAvailable;
+  }
 
-    public Bank setName(String name) {
-        this.name = name;
-        return this;
-    }
+  public Bank setNickname(String nickname) {
+    this.nickname = nickname;
+    return this;
+  }
 
-    public Bank setPersianName(String persianName) {
-        this.persianName = persianName;
-        return this;
-    }
+  public Bank setName(String name) {
+    this.name = name;
+    return this;
+  }
 
-    public Bank setCardNumberIdentifiers(List<Integer> cardNumberIdentifiers) {
-        this.cardNumberIdentifiers = cardNumberIdentifiers;
-        return this;
-    }
+  public Bank setPersianName(String persianName) {
+    this.persianName = persianName;
+    return this;
+  }
 
-    public Bank setIbanCode(String ibanCode) {
-        this.ibanCode = ibanCode;
-        return this;
-    }
+  public Bank setCardNumberIdentifiers(List<Integer> cardNumberIdentifiers) {
+    this.cardNumberIdentifiers = cardNumberIdentifiers;
+    return this;
+  }
 
-    public Bank setAccountNumberAvailable(boolean accountNumberAvailable) {
-        this.accountNumberAvailable = accountNumberAvailable;
-        return this;
-    }
+  public Bank setIbanCode(String ibanCode) {
+    this.ibanCode = ibanCode;
+    return this;
+  }
+
+  public Bank setAccountNumberAvailable(boolean accountNumberAvailable) {
+    this.accountNumberAvailable = accountNumberAvailable;
+    return this;
+  }
 }
--- src/test/java/dev/persiantools/IbanTest.java
+++ src/test/java/dev/persiantools/IbanTest.java
@@ -1,37 +1,35 @@
 package dev.persiantools;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class IbanTest {
 
-    @Test
-    public void shouldValidateIBANsCorrectly(){
-        IbanUtils ibanValidator = new IbanUtils();
+  @Test
+  public void shouldValidateIBANsCorrectly() {
+    IbanUtils ibanValidator = new IbanUtils();
 
-        for(Map.Entry<String, Boolean> entry : getIbans().entrySet()) {
-            String iban = entry.getKey();
-            Boolean assertion = entry.getValue();
+    for (Map.Entry<String, Boolean> entry : getIbans().entrySet()) {
+      String iban = entry.getKey();
+      Boolean assertion = entry.getValue();
 
-            assertEquals((ibanValidator.isValid(iban)), assertion);
-        }
-
+      assertEquals((ibanValidator.isValid(iban)), assertion);
     }
+  }
 
-    private HashMap<String, Boolean> getIbans(){
-        HashMap<String, Boolean> ibanCollection = new HashMap<>();
+  private HashMap<String, Boolean> getIbans() {
+    HashMap<String, Boolean> ibanCollection = new HashMap<>();
 
-        ibanCollection.put("IR820540102680020817909002", true);
-        ibanCollection.put("IR062960000000100324200001", true);
-        ibanCollection.put("IR580540105180021273113007", true);
-        ibanCollection.put("IR012345678901234567890123", false);
-        ibanCollection.put("IR01234567890123456789", false);
-        ibanCollection.put("IR012345678901234567890123456789", false);
+    ibanCollection.put("IR820540102680020817909002", true);
+    ibanCollection.put("IR062960000000100324200001", true);
+    ibanCollection.put("IR580540105180021273113007", true);
+    ibanCollection.put("IR012345678901234567890123", false);
+    ibanCollection.put("IR01234567890123456789", false);
+    ibanCollection.put("IR012345678901234567890123456789", false);
 
-        return ibanCollection;
-    }
+    return ibanCollection;
+  }
 }

08a1f3f11cb695837a3e3acc42deecc0a9922e0b
Mohammad Hosein Mazinani <mhmazinani1994@gmail.com>
2021-04-19 02:10:11 +0430

    feat: get place by national id

--- src/main/java/dev/persiantools/NationalId.java
+++ src/main/java/dev/persiantools/NationalId.java
@@ -1,5 +1,9 @@
 package dev.persiantools;
 
+import dev.persiantools.commons.*;
+import dev.persiantools.usecases.collections.*;
+
+import java.util.*;
 import java.util.regex.*;
 public class NationalId {
 
@@ -46,6 +50,26 @@ public class NationalId {
 
     public static String padLeftZeros(String inputString) {
         return ("00" + inputString).substring(inputString.length() + 2 - 10);
+    }
+
+    /**
+     * Get Province and City name by Iranian National-Id
+     * @param nationalId string of national id - like this: 1111111111
+     * @return An {@link Optional} of {@link Hometown} if nationalId is valid
+     * @throws IllegalArgumentException if nationalId is invalid
+     */
+    public static Optional<Hometown> getPlaceByIranNationalId(String nationalId) {
+
+        if (nationalId == null || nationalId.isBlank())
+            throw new IllegalArgumentException("Null or empty nationalId");
+
+        Pattern pattern = Pattern.compile("^\\d{10}$");
+        if (!pattern.matcher(nationalId).matches())
+            throw new IllegalArgumentException("Invalid nationalId");
+
+        String hometownCode = nationalId.substring(0, 3);
+        Hometown hometown = HometownCollection.getInstance().find(hometownCode);
+        return Optional.ofNullable(hometown);
     }
 
 }
--- src/test/java/dev/persiantools/NationalIdTests.java
+++ src/test/java/dev/persiantools/NationalIdTests.java
@@ -63,4 +63,45 @@ public class NationalIdTests {
         assert NationalId.verifyIranianNationalId("0200203241");
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getPlaceByIranNationalId_NullNationalId_ThrowsException() {
+        NationalId.getPlaceByIranNationalId(null);
+        fail();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getPlaceByIranNationalId_BlankNationalId_ThrowsException() {
+        NationalId.getPlaceByIranNationalId(" ");
+        fail();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getPlaceByIranNationalId_InvalidFormatNationalId_ThrowsException() {
+        NationalId.getPlaceByIranNationalId("some invalid national id");
+        fail();
+    }
+
+    @Test
+    public void getPlaceByIranNationalId_InvalidNationalId_ReturnsEmptyOptional() {
+        assert NationalId.getPlaceByIranNationalId("0008454594").isEmpty();
+        assert NationalId.getPlaceByIranNationalId("8881234567").isEmpty();
+    }
+
+    @Test
+    public void getPlaceByIranNationalId_ValidNationalId_ReturnsHometownOptional() {
+        assert NationalId.getPlaceByIranNationalId("0499370899").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0790419904").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0084575948").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0060495219").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0671658506").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0671658506").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0643005846").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0906582709").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0451727304").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0371359058").isPresent();
+        assert NationalId.getPlaceByIranNationalId("5049478618").isPresent();
+        assert NationalId.getPlaceByIranNationalId("2110990147").isPresent();
+        assert NationalId.getPlaceByIranNationalId("0084545943").isPresent();
+    }
 }
